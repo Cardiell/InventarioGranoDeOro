5d0c6e2cfcb6c1ba4dd1f6c4b935f26b673bd0d3
Victor Sanchez <victorsanchez_94raw@hotmail.com>
2019-03-25 12:06:15 -0700

    Conexion a red y SQL lista

--- app/src/main/java/com/example/inventariogranodeoro/MainActivity.java
+++ app/src/main/java/com/example/inventariogranodeoro/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.inventariogranodeoro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.sql.Connection;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_get;
+    Connection conectar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_get=(Button)findViewById(R.id.button_0);
+
+       /*PRUEBA DE CONEXION
+        btn_get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConexionBDClass con=new ConexionBDClass();
+                conectar=con.Conexion();
+                if (conectar ==null){
+                    Toast.makeText(getApplicationContext(),"Fuera de red",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Dentro de red",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+*/
+
     }
 }
